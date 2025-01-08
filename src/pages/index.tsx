753380851e0e1afd9d45a16646a0d240f93d6a87
Jordi García Ventura <jordigarciaventura@gmail.com>
2025-01-08 12:20:53 +0100

    feat: add umami tracking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Home from '../components/Home'
 import Page from '@shared/Page'
 import { useRouter } from 'next/router'
 import { useMarketMetadata } from '@context/MarketMetadata'
+import Script from 'next/script'
 
 export default function PageHome(): ReactElement {
   const { siteContent } = useMarketMetadata()
@@ -10,11 +11,12 @@ export default function PageHome(): ReactElement {
 
   return (
     <Page
-      title={siteContent?.siteTitle}
-      description={siteContent?.siteTagline}
-      uri={router.route}
-      headerCenter
+    title={siteContent?.siteTitle}
+    description={siteContent?.siteTagline}
+    uri={router.route}
+    headerCenter
     >
+      <Script defer src="https://umami.agrospai.udl.cat/umami" data-website-id="584819b4-4484-4ba7-a74a-0cdfefbdb0fd" />
       <Home />
     </Page>
   )
