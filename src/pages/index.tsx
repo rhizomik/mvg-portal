471f71cc51d172290bcbd8bafa7d39e0a0401270
Jordi García Ventura <jordigarciaventura@gmail.com>
2025-01-08 12:20:53 +0100

    feat: add umami tracking
    Update index.tsx
    
    Change umami id
    
    Update umami tracking code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Home from '../components/Home'
 import Page from '@shared/Page'
 import { useRouter } from 'next/router'
 import { useMarketMetadata } from '@context/MarketMetadata'
+import Script from 'next/script'
 
 export default function PageHome(): ReactElement {
   const { siteContent } = useMarketMetadata()
@@ -15,6 +16,11 @@ export default function PageHome(): ReactElement {
       uri={router.route}
       headerCenter
     >
+      <Script
+        defer
+        src="https://umami.agrospai.udl.cat/umami"
+        data-website-id="dd68db0f-4cc5-4114-b3b2-9d9621a7ce27"
+      />
       <Home />
     </Page>
   )
